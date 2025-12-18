e49e2b91255e44faf8b6f9f9e116756f15935e74
JoshYod <joshuadayne@icloud.com>
2025-12-18 12:57:40 +0000

    make dialogue method for Dave

--- Dave.java
+++ Dave.java
@@ -16,15 +16,16 @@ class Dave extends NPC{
         while(true){
 		System.out.println("-=-=-<Dave>-=-=-");
 		System.out.print("""
-1) Talk
-2) Leave
-""");
+			1. Talk
+			0. Leave
+			"""
+			);
 
 		String choice = getChoice();
 		if(choice.equals("1")){
 			startDialogue();
 		}//end if
-		else if(choice.equals("2")){
+		else if(choice.equals("0")){
 			break;
 		}//end elif
 		else{
@@ -35,9 +36,28 @@ class Dave extends NPC{
 
     public void startDialogue(){
         while(true){
-		System.out.println("------------------------------");
-		
-		break;
+		System.out.println("""
+				------------------------------
+				1. What is this place?
+				2. Who are you?
+				0. Leave
+				"""
+				);
+		String choice = getChoice();
+		if(choice.equals("0")){
+			break;
+		}//end if
+		else if(choice.equals("1")){
+			System.out.println("This is my house... what are you doing here?");
+			enterToContinue();
+		}//end elif
+		else if(choice.equals("2")){
+			System.out.println("I could be asking you the same question.");
+			enterToContinue();
+		}//end elif
+		else{
+			runError("ENTER A VALID OPTION");
+		}//end else
 	}//end while
     }//end startDialogue
 
